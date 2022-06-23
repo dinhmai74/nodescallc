a26145d00d104be0c880712785618ad0ffd08bc6
dinhmai74 <anh.dinh74@gmail.com>
2022-06-23 12:02:26 +0700

    add comments

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const convertToStringFromListObjs = (listObjs) => {
   for (let i = 1; i < listObjs.length; i++) {
     const ele = listObjs[i];
 
+    // valid continue node -> append it to the dicLevel
     if (ele.x - current.x === nodeDistance) {
       dicLevel[ele.x] = `${dicLevel[ele.x - 1]}${ele.name}`;
       current = ele;
       continue;
     }
 
+    // not valid node node and not escape from current tree -> push the last one to result and replace the name of old node
     if (dicLevel[ele.x]) {
       result.push(dicLevel[current.x]);
       if (i === listObjs.length - 1) {
@@ -28,6 +30,7 @@ const convertToStringFromListObjs = (listObjs) => {
       continue;
     }
 
+    // not valid not and escape from current tree -> reset the dicLevel and start from the beginning
     if (dicLevel[current.x]) result.push(dicLevel[current.x]);
     dicLevel = {
       [ele.x]: ele.name,
